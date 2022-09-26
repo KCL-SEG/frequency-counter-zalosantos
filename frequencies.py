7fd30ba3cfd2fed497d4358cd067e651933ce759
Gonçalo  Azevedo <zalosantos02@gmail.com>
2022-09-26 17:26:37 +0100

    Complete I hope

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    frequencies = {}  #dictionary
+    x=0
+    n = len(items)
+    while x < n:   #it scans the all the items in the list
+
+       scanner = False
+       if frequencies.get(str(items[x])) == None:
+           i= frequencies.get(str(items[x]))
+           i=0
+           frequencies[str(items[x])]= i+1
+
+
+       else:
+           frequencies[str(items[x])]= frequencies.get(str(items[x]))+1
+       x=x+1
     return frequencies
